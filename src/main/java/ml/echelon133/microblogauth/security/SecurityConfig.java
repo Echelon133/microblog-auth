43e59f862b4da7e0581fca6f42effaf530f6204b
Mateusz <echelon133@gmail.com>
2021-03-23 00:40:26 +0100

    Set up SecurityConfig - cors, user details service, basic auth

---
+++ src/main/java/ml/echelon133/microblogauth/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package ml.echelon133.microblogauth.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private PasswordEncoder passwordEncoder;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder,
+                          @Qualifier("customUserDetailsService") UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/token/renew").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/token").hasRole("USER")
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
