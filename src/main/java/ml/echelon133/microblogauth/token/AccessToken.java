fddf4a00264562940cbc121727e5656457269ab3
Mateusz <echelon133@gmail.com>
2021-03-31 19:39:21 +0200

    Make access/refresh token entities store user's roles

--- src/main/java/ml/echelon133/microblogauth/token/AccessToken.java
+++ src/main/java/ml/echelon133/microblogauth/token/AccessToken.java
@@ -3,9 +3,13 @@ package ml.echelon133.microblogauth.token;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 // access tokens should expire after 60 minutes
 @RedisHash(value="accessToken", timeToLive = 3600)
@@ -16,6 +20,7 @@ public class AccessToken implements Serializable {
     @Id
     private String token;
     private UUID ownerUuid;
+    private List<String> roles;
 
     @Indexed
     private String ownerUsername;
@@ -24,6 +29,19 @@ public class AccessToken implements Serializable {
         this.token = TokenGenerator.generateToken(ACCESS_TOKEN_LENGTH);
         this.ownerUuid = ownerUuid;
         this.ownerUsername = ownerUsername;
+    }
+
+    public AccessToken(UUID ownerUuid, String ownerUsername, Collection<? extends GrantedAuthority> auth) {
+        this(ownerUuid, ownerUsername);
+        this.roles = auth
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+    }
+
+    public AccessToken(UUID ownerUuid, String ownerUsername, List<String> roles) {
+        this(ownerUuid, ownerUsername);
+        this.roles = roles;
     }
 
     public String getToken() {
@@ -48,5 +66,13 @@ public class AccessToken implements Serializable {
 
     public void setOwnerUsername(String ownerUsername) {
         this.ownerUsername = ownerUsername;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 }
--- src/main/java/ml/echelon133/microblogauth/token/RefreshToken.java
+++ src/main/java/ml/echelon133/microblogauth/token/RefreshToken.java
@@ -3,9 +3,14 @@ package ml.echelon133.microblogauth.token;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 // refresh tokens should expire after 30 days
 @RedisHash(value="refreshToken", timeToLive = 2592000)
@@ -16,14 +21,20 @@ public class RefreshToken implements Serializable {
     @Id
     private String token;
     private UUID ownerUuid;
+    private List<String> roles;
 
     @Indexed
     private String ownerUsername;
 
-    public RefreshToken(UUID ownerUuid, String ownerUsername) {
+    public RefreshToken() {}
+    public RefreshToken(UUID ownerUuid, String ownerUsername, Collection<? extends GrantedAuthority> auth) {
         this.token = TokenGenerator.generateToken(REFRESH_TOKEN_LENGTH);
         this.ownerUuid = ownerUuid;
         this.ownerUsername = ownerUsername;
+        this.roles = auth
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
     }
 
     public String getToken() {
@@ -48,5 +59,13 @@ public class RefreshToken implements Serializable {
 
     public void setOwnerUsername(String ownerUsername) {
         this.ownerUsername = ownerUsername;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 }
